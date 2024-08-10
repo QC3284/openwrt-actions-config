d6e96ce366e70fafb3672d5b105468416a7ce8b1
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-08-10 18:21:02 +0800

    Create apt-xxx.sh

---
+++ apt-xxx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo $(uname -a) 
+sleep 5
+sudo apt-get update
+sudo apt update
+sudo apt upgrade -y
+sudo apt update
+sudo apt install build-essential ecj fastjar file flex g++ gcc-multilib g++-multilib gawk gettext git git-core java-propose-classpath libelf-dev libncurses5-dev libncursesw5-dev libssl-dev swig python3 python3-dev python3-distutils python3-pyelftools subversion unzip wget zlib1g-dev rsync qemu-utils zip -y
+sudo apt-get update
+sudo apt-get install -y libfuse-dev neofetch 7zip
+sudo apt update
+sudo apt full-upgrade -y
+sudo neofetch
+sleep 10
