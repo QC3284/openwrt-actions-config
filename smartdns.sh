b978664f96dbee5ab61736dea4d2b60252fbd03f
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-08-22 18:24:14 +0800

    Create smartdns.sh

---
+++ smartdns.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://github.com/pymumu/smartdns/archive/refs/heads/master.zip -o ""smartdns-master.zip" & curl https://github.com/pymumu/luci-app-smartdns/archive/refs/heads/master.zip -o "luci-app-smartdns-master.zip"
+curl https://github.com/pymumu/openwrt-smartdns/archive/refs/heads/master.zip -o "openwrt-smartdns-master.zip"
