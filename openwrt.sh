da3fea0c6e3894ab7033e269989f3d795ef38ffb
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-08-22 18:13:52 +0800

    Create openwrt.sh

---
+++ openwrt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl https://github.com/openwrt/openwrt/archive/refs/heads/main.zip -o "openwrt-main.zip" & curl https://github.com/openwrt/openwrt/archive/refs/heads/lede-17.01.zip -o "openwrt-lede-17.01.zip"
+curl https://github.com/openwrt/openwrt/archive/refs/heads/master.zip -o "openwrt-master.zip" & curl https://github.com/openwrt/openwrt/archive/refs/heads/openwrt-18.06.zip -o "openwrt-18.06.zip"
+curl https://github.com/openwrt/openwrt/archive/refs/heads/openwrt-19.07.zip -o "openwrt-19.07.zip" & curl https://github.com/openwrt/openwrt/archive/refs/heads/openwrt-21.02.zip -o "openwrt-21.02.zip"
+curl https://github.com/openwrt/openwrt/archive/refs/heads/openwrt-22.03.zip -o "openwrt-22.03.zip" & curl https://github.com/openwrt/openwrt/archive/refs/heads/openwrt-23.05.zip -o "openwrt-23.05.zip"
