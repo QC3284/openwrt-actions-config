dd0a955f1c5c69482b301962a0bcce7818bf1f2b
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-08-22 22:22:28 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://github.com/coolsnowwolf/lede/archive/refs/heads/master.zip -o "coolsnowwolf-lede-master.zip" & curl https://github.com/coolsnowwolf/packages/archive/refs/heads/master.zip -o "coolsnowwolf-packages-master.zip"
+curl https://github.com/coolsnowwolf/lede/archive/refs/heads/master.zip -o "coolsnowwolf-lede-master.zip" & curl https://github.com/coolsnowwolf/packages/archive/refs/heads/master.zip -o "coolsnowwolf-lede-packages-master.zip"
