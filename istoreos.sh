e8141ff873c5e11eda7d4a6ad1fceb93c2d9d0a1
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-08-22 18:20:38 +0800

    Create istoreos.sh

---
+++ istoreos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-22.03.zip & curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-21.02.zip
+curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-21.02.2.zip & curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-22.03.4.zip
+curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-22.03.5.zip & curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-22.03.6.zip
+curl https://github.com/istoreos/istoreos/archive/refs/heads/istoreos-23.05.zip & curl https://github.com/istoreos/istoreos/archive/refs/heads/main.zip -o "istoreos-main.zip"
+curl https://github.com/istoreos/istoreos/archive/refs/heads/master.zip -o "istoreos-master.zip" & curl https://github.com/istoreos/istoreos/archive/refs/heads/openwrt-21.02.3.zip -o "istoreos-openwrt-21.02.3.zip"
+curl https://github.com/istoreos/istoreos/archive/refs/heads/openwrt-22.03.4.zip -o "ostoreos-openwrt-22.03.4.zip" & curl https://github.com/istoreos/istoreos/archive/refs/heads/openwrt-22.03.5.zip -o "istoreos-openwrt-22.03.5.zip"
+curl https://github.com/istoreos/istoreos/archive/refs/heads/openwrt-23.05.zip -o "istoreos-openwrt-23.05.zip"
