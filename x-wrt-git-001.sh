9828dbf5c49992d25f24a0ff9865365d8f92bd26
QC3284 <2078725082@qq.com>
2024-08-23 11:50:53 +0800

    创建 x-wrt-git-001.sh

---
+++ x-wrt-git-001.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/garypang13/luci-theme-edge.git
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git
+git clone https://github.com/derisamedia/luci-theme-alpha.git
+git clone https://github.com/derisamedia/luci-app-alpha-config.git
+git clone https://github.com/f8q8/luci-app-filetransfer.git
+git clone https://github.com/kongfl888/luci-app-timedreboot.git
+git clone https://github.com/sirpdboy/luci-app-netdata.git
+git clone https://github.com/zzsj0928/luci-app-pushbot.git
+git clone https://github.com/animegasan/luci-app-wolplus.git
+git clone https://github.com/sirpdboy/luci-app-eqosplus.git
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git ddns-go
+git clone https://github.com/QC3284/coremark-openwrt.git utils/coremark
+git clone https://github.com/QC3284/luci-app-adguardhome.git
+git clone https://github.com/muink/luci-app-netspeedtest.git
