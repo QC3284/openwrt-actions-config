7ea217cae0f0435a6b5527df7fcfde465f7cefdb
QC3284 <2078725082@qq.com>
2024-09-07 23:02:03 +0800

    update x-wrt-actions-txt-001.sh

--- x-wrt-actions-txt-001.sh
+++ x-wrt-actions-txt-001.sh
@@ -3,7 +3,7 @@ echo "自动编译 (Automatic compilation)" >> release.txt
 echo "使用源码 (Use source code)：" >> release.txt
 echo "[x-wrt](https://github.com/x-wrt/x-wrt)" >> release.txt
 echo "使用标签 (Use tags)" >> release.txt
-echo "[24.04_b202409010000](https://github.com/x-wrt/x-wrt/releases/tag/24.04_b202409010000)" >> release.txt
+echo "[24.04_b202409070905](https://github.com/x-wrt/x-wrt/releases/tag/24.04_b202409070905)" >> release.txt
 echo "默认账号/密码 (Default account/password)：" >> release.txt
 echo "admin/admin" >> release.txt
 echo "SSH默认账号/密码 (Default SSH account/password)：" >> release.txt
@@ -23,4 +23,4 @@ echo "- 如果发现本次编译没有文件，说明正在调整插件和更新
 echo "- 如果发现更新频繁，尽量不要下载（可能正在调整插件）" >> release.txt
 echo "- 此固件基于[X-WRT](https://github.com/x-wrt/x-wrt)源码编译，加入了一些插件，如需下载官方原版，请到：" >> release.txt
 echo "[X-WRT/OpenWrt/LEDE 固件下载](https://downloads.x-wrt.com/rom)" >> release.txt
-echo "- 本仓库禁止克隆到[GitCode](https://gitcode.com)" >> release.txt
+echo "- 本仓库没有在[GitCode](https://gitcode.com)上发布" >> release.txt
