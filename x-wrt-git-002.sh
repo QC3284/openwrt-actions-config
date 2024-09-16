653808da7f768d3811e3a621c44922ee1702a70f
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-09-16 20:42:44 +0800

    Update x-wrt-git-002.sh

--- x-wrt-git-002.sh
+++ x-wrt-git-002.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
 sleep 3
