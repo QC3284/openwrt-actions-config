e3da33fbeeae410ef2df252c5750184d215b4541
QC3284 <2078725082@qq.com>
2024-10-03 21:53:43 +0800

    New

---
+++ lede-github-actions-feed-01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#本仓库及文件只在Github发布
+#作者：QC3284@github.com(https://github.com/QC3284)
+#本仓库地址：https://github.com/QC3284/openwrt-actions-config
+#最后更新时间：2024.10.03
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
---
+++ lede-github-actions-feed-02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#本仓库及文件只在Github发布
+#作者：QC3284@github.com(https://github.com/QC3284)
+#本仓库地址：https://github.com/QC3284/openwrt-actions-config
+#最后更新时间：2024.10.03
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
