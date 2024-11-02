d24e1a5f4e564684cc0fe2a33d0a4ac21daec325
QC3284 <2078725082@qq.com>
2024-11-02 22:57:08 +0800

    Fix

--- openwrtci.sh
+++ openwrtci.sh
@@ -4,7 +4,14 @@
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
 #最后更新时间：2024.11.02
 sudo rm -rf feeds/packages/net/smartdns
+sudo rm -rf feeds/kenzo/smartdns
 sudo rm -rf feeds/luci/applications/luci-app-smartdns
+sudo rm -rf feeds/kenzo/luci-app-smartdns
+sudo rm -rf feeds/luci/applications/luci-app-mosdns
+sudo rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+sudo rm -rf feeds/packages/utils/v2dat
+sudo rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone https://github.com/pymumu/openwrt-smartdns.git  feeds/packages/net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 sleep 3
