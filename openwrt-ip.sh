e9e06d2b9e8dc099309a3f7098dc70bbe70a8a8c
QC3284 <2078725082@qq.com>
2024-11-02 08:51:53 +0800

    update

--- openwrt-ip.sh
+++ openwrt-ip.sh
@@ -3,6 +3,9 @@
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
 #最后更新时间：2024.10.19
-sed -i '1i src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sleep 2
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 sleep 3
--- x-wrt-git-003.sh
+++ x-wrt-git-003.sh
@@ -2,9 +2,16 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.10.03
+#最后更新时间：2024.11.02
 sudo rm -rf feeds/packages/net/smartdns
+sudo rm -rf feeds/kenzo/smartdns
 sudo rm -rf feeds/luci/applications/luci-app-smartdns
+sudo rm -rf feeds/kenzo/luci-app-smartdns
+sudo rm -rf feeds/luci/applications/luci-app-mosdns
+sudo rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+sudo rm -rf feeds/packages/utils/v2dat
+sudo rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone https://github.com/pymumu/openwrt-smartdns.git  feeds/packages/net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 sleep 3
