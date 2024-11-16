9c0c0f0dc123e3a79b2b63581e8717643062f2ff
QC3284 <2078725082@qq.com>
2024-11-16 21:02:05 +0800

    update

--- x-wrt-actions-txt-001.sh
+++ x-wrt-actions-txt-001.sh
@@ -2,7 +2,7 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.11.09
+#最后更新时间：2024.11.16
 echo "自动编译 (Automatic compilation)" >> release.txt
 echo "使用源码 (Use source code)：" >> release.txt
 echo "[x-wrt](https://github.com/x-wrt/x-wrt)" >> release.txt
@@ -26,5 +26,5 @@ echo "- 固件结构应该为: uImage+UBI镜像" >> release.txt
 echo "- 如果发现本次编译没有文件，说明正在调整插件和更新（也有可能是源码炸了）" >> release.txt
 echo "- 如果发现更新频繁，尽量不要下载（可能正在调整插件）" >> release.txt
 echo "- 此固件基于[X-WRT](https://github.com/x-wrt/x-wrt)源码编译，加入了一些插件，如需下载官方原版，请到：" >> release.txt
-echo "[X-WRT/OpenWrt/LEDE 固件下载](https://downloads.x-wrt.com/rom)" >> release.txt
+echo "- [X-WRT/OpenWrt/LEDE 固件下载](https://downloads.x-wrt.com/rom)" >> release.txt
 echo "- 本仓库没有在[GitCode](https://gitcode.com)上发布" >> release.txt
