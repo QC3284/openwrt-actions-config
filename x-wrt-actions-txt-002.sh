9427fe1aeee1170e586937152107630449d8b496
QC3284 <2078725082@qq.com>
2024-11-16 21:12:56 +0800

    update

---
+++ x-wrt-actions-txt-002.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+#本仓库及文件只在Github发布
+#作者：QC3284@github.com(https://github.com/QC3284)
+#本仓库地址：https://github.com/QC3284/openwrt-actions-config
+#最后更新时间：2024.11.16
+echo "自动编译 (Automatic compilation)" >> release.txt
+echo "使用源码 (Use source code)：" >> release.txt
+echo "[x-wrt](https://github.com/x-wrt/x-wrt)" >> release.txt
