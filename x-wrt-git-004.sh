a6afd03129b484165db1a1211bb8b5cc5622da5f
QC3284 <2078725082@qq.com>
2024-11-29 23:42:04 +0800

    update

--- x-wrt-git-004.sh
+++ x-wrt-git-004.sh
@@ -3,14 +3,14 @@
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
 #最后更新时间：2024.11.29
-7zz a -mx=9 -mmt=8 "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606.zip" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
+7zz a -mx=9 -mmt=8 "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606.7z" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
 sleep 3
 sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin
 sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin
 sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin
 sleep 3
-7zz a -mx=9 -mmt=8 "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606.zip" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
-7zz a -mx=9 -mmt=8 "cr660x-pb-boot.zip" "pb-boot.ecc" "pb-boot.img" "pb-boot.sha256"
+7zz a -mx=9 -mmt=8 "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606.7z" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
+7zz a -mx=9 -mmt=8 "cr660x-pb-boot.7z" "pb-boot.ecc" "pb-boot.img" "pb-boot.sha256"
 sleep 3
 sudo rm -rf pb-boot.ecc
 sudo rm -rf pb-boot.img
