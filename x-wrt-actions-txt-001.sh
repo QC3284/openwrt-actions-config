772106882427af937d20098e7e0d868a84d71847
QC3284 <2078725082@qq.com>
2024-11-29 23:05:07 +0800

    update

--- x-wrt-actions-txt-001.sh
+++ x-wrt-actions-txt-001.sh
@@ -2,12 +2,12 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.11.16
+#最后更新时间：2024.11.29
 echo "自动编译 (Automatic compilation)" >> release.txt
 echo "使用源码 (Use source code)：" >> release.txt
 echo "[x-wrt](https://github.com/x-wrt/x-wrt)" >> release.txt
 echo "使用标签 (Use tags)" >> release.txt
-echo "[24.04_b202411071136](https://github.com/x-wrt/x-wrt/releases/tag/24.04_b202411071136)" >> release.txt
+echo "[24.04_b202411262136](https://github.com/x-wrt/x-wrt/releases/tag/24.04_b202411262136)" >> release.txt
 echo "默认账号/密码 (Default account/password)：" >> release.txt
 echo "admin/admin" >> release.txt
 echo "SSH默认账号/密码 (Default SSH account/password)：" >> release.txt
@@ -17,12 +17,7 @@ echo "- ****-factory.bin 通常用于从原厂固件或Uboot中刷入" >> releas
 echo "- ****-sysupgrade.bin 通常用于web升级" >> release.txt
 echo "- ****-initramfs-kernel.bin 具有最少文件系统的Linux内核. 对于首次安装或恢复很有用" >> release.txt
 echo "- sha256sums 是校验文件" >> release.txt
-echo "- PB-Boot适用于Xiaomi CR6608/CR6609/CR6606" >> release.txt
-echo "- pb-boot.img 是pb-boot(bootloader)文件" >> release.txt
-echo "- pb-boot.sha256是pb-boot的校验文件" >> release.txt
-echo "- pb-boot.ecc是带OOB数据用于nand编程器的pb-boot(bootloader)" >> release.txt
-echo "- 此pb-boot(bootloader)会从NAND Flash的0x200000读取uImage,目前不支持FitImage!!!" >> release.txt
-echo "- 固件结构应该为: uImage+UBI镜像" >> release.txt
+echo "- PB-Boot适用于Xiaomi CR6608/CR6609/CR6606/CR660x" >> release.txt
 echo "- 如果发现本次编译没有文件，说明正在调整插件和更新（也有可能是源码炸了）" >> release.txt
 echo "- 如果发现更新频繁，尽量不要下载（可能正在调整插件）" >> release.txt
 echo "- 此固件基于[X-WRT](https://github.com/x-wrt/x-wrt)源码编译，加入了一些插件，如需下载官方原版，请到：" >> release.txt
--- x-wrt-git-004.sh
+++ x-wrt-git-004.sh
@@ -2,7 +2,7 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.10.06
+#最后更新时间：2024.11.29
 7zz a -mx=9 -mmt=8 "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606.zip" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
 sleep 3
 sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin
@@ -10,7 +10,11 @@ sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysup
 sudo rm -rf openwrt-23.05.5-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin
 sleep 3
 7zz a -mx=9 -mmt=8 "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606.zip" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin" "immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin"
+7zz a -mx=9 -mmt=8 "cr660x-pb-boot.zip" "pb-boot.ecc" "pb-boot.img" "pb-boot.sha256"
 sleep 3
+sudo rm -rf pb-boot.ecc
+sudo rm -rf pb-boot.img
+sudo rm -rf pb-boot.sha256
 sudo rm -rf immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-initramfs-kernel.bin
 sudo rm -rf immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-firmware.bin
 sudo rm -rf immortalwrt-23.05.4-ramips-mt7621-xiaomi_mi-router-cr6606-squashfs-sysupgrade.bin
