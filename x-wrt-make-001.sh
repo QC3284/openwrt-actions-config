ed08ae892927b33a81fcc7c0a99fe3120a0b4eae
QC3284 <2078725082@qq.com>
2024-11-30 18:59:04 +0800

    fix

--- x-wrt-make-001.sh
+++ x-wrt-make-001.sh
@@ -2,8 +2,8 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.10.03
-make download -j8 2>&1 | tee make.log 
+#最后更新时间：2024.11.30
+make download -j8 2>&1 | tee make_download.log 
 find dl -size -1024c -exec ls -l {} \;
 find dl -size -1024c -exec rm -f {} \;
 sleep 3
