b64c00eced8a9c2f46f52a26a2e9a69edb0ac699
QC3284 <2078725082@qq.com>
2024-12-30 11:46:53 +0800

    update

--- x-wrt-git-001.sh
+++ x-wrt-git-001.sh
@@ -2,11 +2,18 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.10.03
+#最后更新时间：2024.12.31
+git clone https://github.com/kiddin9/kwrt-packages.git opldf
+sleep 2
+cp -r opldf/coremark package/utils/
+cp -r opldf/adguardhome feeds/packages/net/
+cp -r opldf/luci-app-adguardhome package/
+sleep 2
+sudo rm -rf opldf
+cd package
 git clone https://github.com/garypang13/luci-theme-edge.git
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git
 git clone https://github.com/derisamedia/luci-theme-alpha.git
 git clone https://github.com/derisamedia/luci-app-alpha-config.git
 git clone https://github.com/f8q8/luci-app-filetransfer.git
@@ -14,7 +21,7 @@ git clone https://github.com/sirpdboy/luci-app-netdata.git
 git clone https://github.com/zzsj0928/luci-app-pushbot.git
 git clone https://github.com/animegasan/luci-app-wolplus.git
 git clone https://github.com/sirpdboy/luci-app-eqosplus.git
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git ddns-go
-git clone https://github.com/QC3284/coremark-openwrt.git utils/coremark
-git clone https://github.com/QC3284/luci-app-adguardhome.git
+git clone https://github.com/sirpdboy/luci-app-lucky.git lucky
+echo "Done"
 sleep 3
+
