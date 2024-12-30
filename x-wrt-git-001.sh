0b78b5fc799e9cdbaacb7e12fde8db05082a624b
QC3284 <2078725082@qq.com>
2024-12-30 12:36:23 +0800

    fix

--- x-wrt-git-001.sh
+++ x-wrt-git-001.sh
@@ -5,24 +5,11 @@
 #最后更新时间：2024.12.31
 git clone https://github.com/kiddin9/kwrt-packages.git opldf
 sleep 2
-cp -r opldf/coremark package/utils/
-cp -r opldf/adguardhome feeds/packages/net/
-cp -r opldf/luci-app-adguardhome package/
+cp -r opldf/coremark openwert/package/utils/
+cp -r opldf/adguardhome openwrt/feeds/packages/net/
+cp -r opldf/luci-app-adguardhome openwrt/package/
 sleep 2
 sudo rm -rf opldf
-cd package
-git clone https://github.com/garypang13/luci-theme-edge.git
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/derisamedia/luci-theme-alpha.git
-git clone https://github.com/derisamedia/luci-app-alpha-config.git
-git clone https://github.com/f8q8/luci-app-filetransfer.git
-git clone https://github.com/sirpdboy/luci-app-netdata.git
-git clone https://github.com/zzsj0928/luci-app-pushbot.git
-git clone https://github.com/animegasan/luci-app-wolplus.git
-git clone https://github.com/sirpdboy/luci-app-eqosplus.git
-git clone https://github.com/sirpdboy/luci-app-lucky.git lucky
-git clone https://github.com/QC3284/luci-app-opkg-autoupdate.git
 echo "Done"
 sleep 3
 
