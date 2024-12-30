73e2e1ec4b57e269c7a84ba886904f8b234ccaac
QC3284 <2078725082@qq.com>
2024-12-30 17:08:04 +0800

    update

--- x-wrt-git-001.sh
+++ x-wrt-git-001.sh
@@ -5,9 +5,6 @@
 #最后更新时间：2024.12.31
 git clone https://github.com/kiddin9/kwrt-packages.git opldf
 sleep 2
-sudo rm -rf package/utils/coremark
 cp -r opldf/coremark package/utils/
-cp -r opldf/luci-app-adguardhome package/
 echo "Done"
 sleep 3
-sudo rm -rf opldf
---
+++ x-wrt-git-005.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#本仓库及文件只在Github发布
+#作者：QC3284@github.com(https://github.com/QC3284)
+#本仓库地址：https://github.com/QC3284/openwrt-actions-config
+#最后更新时间：2024.12.31
+sudo rm -rf feeds/luci/applications/luci-app-adguardhome
+sudo rm -rf feeds/luci/applications/luci-app-ramfree
+sudo rm -rf feeds/luci/applications/luci-app-vlmcsd
+sleep 2
+cp -r opldf/luci-app-adguardhome package/
+cp -r opldf/luci-app-ramfree package/
+cp -r opldf/luci-app-vlmcsd package/
+cp -r opldf/vlmcsd feeds/packages/net/
+sudo rm -rf opldf
+echo "Done"
+slepp 3
