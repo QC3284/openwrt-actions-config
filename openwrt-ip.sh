4e6181bcc56b7d4f541b90410338acd20af9839e
QC3284 <2078725082@qq.com>
2025-02-05 13:22:04 +0800

    Fix

--- openwrt-ip.sh
+++ openwrt-ip.sh
@@ -2,13 +2,13 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2025.2.4
+#最后更新时间：2025.2.5
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sleep 2
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 git clone --branch master --single-branch https://github.com/vernesong/OpenClash.git clash
 cp -r clash/luci-app-openclash package/luci-app-openclash
-sudo rm -rf luci-app-openclash
+sudo rm -rf clash
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
