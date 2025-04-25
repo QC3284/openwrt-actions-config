cf97503394eca5cbc7f5c6b8e5579f726f388d7d
QC3284 <2078725082@qq.com>
2025-04-26 01:51:42 +0800

    FIX

--- x-wrt-git-001.sh
+++ x-wrt-git-001.sh
@@ -2,9 +2,11 @@
 #本仓库及文件只在Github发布
 #作者：QC3284@github.com(https://github.com/QC3284)
 #本仓库地址：https://github.com/QC3284/openwrt-actions-config
-#最后更新时间：2024.12.31
+#最后更新时间：2025.4.26
 git clone https://github.com/kiddin9/kwrt-packages.git opldf
 sleep 2
+sudo rm -rf package/utils/coremark
 cp -r opldf/coremark package/utils/
 echo "Done"
 sleep 3
+sudo rm -rf opldf
